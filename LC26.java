48cffaeb91d2a8d32596b1ca867faf8c2dd5b374
Ayush <ayushparmar989@gmail.com>
2024-06-12 23:56:57 +0530

    LC26

---
+++ LC26.java
@@ -0,0 +1,16 @@
+public class LC26 {
+    public static int removeDuplicate(int[] arr){
+        int i=0;
+        for (int j=0; j<arr.length; j++){
+            arr[i++]= arr[j];
+            while(j != arr.length-1 && arr[j] == arr[j+1]){
+                j++;
+            }
+        }
+        return i;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 2, 2, 3, 3, 4, 4};
+        System.out.print(removeDuplicate(arr));
+    }
+}
