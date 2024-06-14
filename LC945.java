f44e876051bdd57685fbb2f770a9c5d19cf17f8a
Ayush <ayushparmar989@gmail.com>
2024-06-15 00:42:47 +0530

    Time complexity & new approach aded

--- LC945.java
+++ LC945.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 // 945. Minimum increment to make array unique.
 public class LC945 {
+    //----------------------->Time Complexity Exceeds.<----------------------
     /*public static int minIncrementForUnique(int[] arr){
         Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
@@ -19,9 +20,10 @@ public class LC945 {
             }
         }
         return count;
-    }*/
+    }*
 
-    public static int minIncrementForUnique(int[] arr){
+    //-------------------->Time Complexity : O(n.log(n)) <----------------------
+    /*public static int minIncrementForUnique(int[] arr){
         Arrays.sort(arr);
         int count = 0;
         for (int i =1; i<arr.length; i++){
@@ -31,7 +33,26 @@ public class LC945 {
             }
         }
         return count;
+    }*/
+
+    //-------------------->Time Complexity : O(n) <----------------------
+    public static int minIncrementForUnique(int[] arr){
+        int[] frequency = new int[arr.length + Arrays.stream(arr).max().getAsInt()];
+        int ans =0;
+        for (int val : arr){
+            frequency[val]++;
+        }
+        for (int i=0; i< frequency.length; i++){
+            if (frequency[i]<= 1) continue;
+
+            int duplicate = frequency[i] -1;
+            frequency[i+1] += duplicate;
+            frequency[i] = 1;
+            ans += duplicate;
+        }
+        return ans;
     }
+
     public static void main(String[] args) {
         int[] arr = {3, 2, 1, 2, 3, 7};
         // sorted array--> 1, 2, 2, 3, 3, 7
